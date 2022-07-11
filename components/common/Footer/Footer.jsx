c6e276ef0ee322367007cbb4988bb0af18be10a5
Andrew Chichvarin <acula.sckalapendra@yandex.ru>
2022-07-11 19:11:41 +0300

    Updated the Footer Component and added an icon

--- components/common/Footer/Footer.jsx
+++ components/common/Footer/Footer.jsx
@@ -1,11 +1,26 @@
 import React from "react";
-import { Container } from "../../ui";
+import { Github } from "../../icon";
+import { Container, Logo } from "../../ui";
 import s from "./Footer.module.css";
 
 const Footer = () => {
   return (
     <footer className={s.root}>
-      <Container>Footer</Container>
+      <Container>
+        <div className={s.footerContainer}>
+          <div>
+            <a href="https://github.com/zumpro/next-meteo">
+              <Github className="w-6 h-6" />
+            </a>
+          </div>
+          <div className="text-gray-400 float-right">
+            <span className="cursor-pointer underline hover:no-underline">
+              User agreement.
+            </span>
+            <span className="ml-1">Created by Andrew Chichvarin</span>
+          </div>
+        </div>
+      </Container>
     </footer>
   );
 };
--- components/icon/index.js
+++ components/icon/index.js
@@ -1,2 +1,3 @@
 export { default as Search } from "./Search";
 export { default as LocationMarker } from "./LocationMarker";
+export { default as Github } from "./Github";
