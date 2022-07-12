0512e0377f8defc0a4c1d14a3c7b81b8cbd5b031
Andrew Chichvarin <acula.sckalapendra@yandex.ru>
2022-07-12 20:35:25 +0300

    Updated Searchbar Component and Forecast Page

--- components/common/Navbar/Navbar.jsx
+++ components/common/Navbar/Navbar.jsx
@@ -10,48 +10,21 @@ const Navbar = () => {
 
   // Button State
   const [loading, setLoading] = useState(false);
-  // Wather State
-  const [weatherData, setWeatherData] = useState([]);
-
-  const getWeather = async (location) => {
-    setWeatherData([]);
-
-    let howToSearch =
-      typeof location === "string"
-        ? `q=${location}`
-        : `lat=${location[0]}&lon=${location[1]}`;
-    try {
-      let res = await fetch(
-        `${process.env.NEXT_PUBLIC_API_URL + howToSearch}&appid=${
-          process.env.NEXT_PUBLIC_API_KEY
-        }&lang=ru`
-      );
-      let data = await res.json();
-      setWeatherData(data);
-
-      router.push(
-        {
-          pathname: `/forecast`,
-          query: `lat=${location[0]}&lon=${location[1]}`,
-        },
-        undefined,
-        { shallow: true }
-      );
-    } catch (error) {
-      console.log(error);
-    }
-  };
 
   const myIP = (location) => {
     setLoading(true);
-
     const { latitude, longitude } = location.coords;
-    getWeather([latitude, longitude]);
-
+    router.push(
+      {
+        pathname: `/forecast`,
+        query: `lat=${latitude}&lon=${longitude}`,
+      },
+      undefined,
+      { shallow: true }
+    );
     setLoading(false);
   };
 
-  console.log(weatherData);
   return (
     <header className={s.root}>
       <Container>
--- pages/forecast.js
+++ pages/forecast.js
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Layout } from "../components/common";
+import { useRouter } from "next/router";
 
 export default function Forecast() {
-  return <div>forecast</div>;
+  const [weatherData, setWeatherData] = useState([]);
+  const router = useRouter();
+
+  useEffect(() => {
+    getWeather();
+    console.log("Hello");
+  }, [router.query]);
+
+  const getWeather = async () => {
+    setWeatherData([]);
+    const { q, lat, lon } = router.query;
+
+    let howToSearch =
+      typeof q === "string" ? `q=${q}` : `lat=${lat}&lon=${lon}`;
+    try {
+      let res = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL + howToSearch}&appid=${
+          process.env.NEXT_PUBLIC_API_KEY
+        }&lang=ru`
+      );
+      let data = await res.json();
+      setWeatherData(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  console.log(weatherData);
+
+  return (
+    <div>
+      <h1>forecast</h1>
+    </div>
+  );
 }
 
 Forecast.Layout = Layout;
