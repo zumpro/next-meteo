58b68e18853a2e232bf76458622b275766f2ceee
Andrew Chichvarin <acula.sckalapendra@yandex.ru>
2022-11-28 22:23:21 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -23,14 +23,16 @@ export default function Home() {
 
     setLoading(false);
   };
+  let info = process.env.NEXT_PUBLIC_ICON_URL;
+
+  console.log(info);
 
   return (
     <div className="absolute w-full h-full flex items-center">
       <div className=" relative w-full">
         <div className="text-center max-w-[720px] m-auto px-4 md:px-0">
           <h1 className="text-3xl md:text-5xl font-bold text-gray-300">
-            Введите название города, чтобы узнать погоду{" "}
-            {process.env.NEXT_PUBLIC_ICON_URL}
+            Введите название города, чтобы узнать погоду
           </h1>
           <p className="text-lg md:text-2xl font-normal text-gray-300 mt-4">
             или нажмите на кнопку
